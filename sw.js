b81022dcbda50ef29d7c9f9b166e950fdf0516fb
ark-pkr <mrogerio4578@gmail.com>
2026-02-12 17:58:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 const CACHE_NAME = 'cine-ark-v1';
 const ASSETS = [
-  './',
   './index.html',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/vue@3/dist/vue.global.js',
-  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// Instalação e Cache dos arquivos essenciais
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Estratégia: Tenta rede, se falhar, usa cache
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
